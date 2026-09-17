289dab23bad2b4925811b512e1195b1e24b0658d
Menna Said <mennasaid091@gmail.com>
2026-09-17 05:20:49 -0400

    Add EmotionDetection package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,18 @@
+import requests, json
+
+def emotion_detector(text_to_analyze):
+    url='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response=requests.post(url, headers=headers, json={ "raw_document": { "text": text_to_analyze }} )
+    response_dict=json.loads(response.text)
+    emotions= response_dict["emotionPredictions"][0]["emotion"]
+    dominant_emotion=max(emotions, key=emotions.get)
+    anger=emotions["anger"]
+    disgust=emotions["disgust"]
+    fear=emotions["fear"]
+    joy=emotions["joy"]
+    sadness=emotions["sadness"]
+    emotions["dominant_emotion"]=dominant_emotion
+    
+    return emotions
+
